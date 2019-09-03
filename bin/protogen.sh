e84da2a4b88be634e33dd7c52084b8012f7733f6
jace-ys <jaceys.tan@gmail.com>
2019-09-03 23:57:59 +0100

    Tweak protogen.sh

--- bin/protogen.sh
+++ bin/protogen.sh
@@ -39,7 +39,7 @@ for target in "${ADDR[@]}"; do
     ${PROTOPATH}/${target}.proto
   echo "==> Generated gRPC stub from ${target}.proto"
 
-  if ${GATEWAY}; then
+  if [ "$GATEWAY" = true ]; then
     protoc \
       -I/usr/local/include \
       -I${PROTOPATH} \
